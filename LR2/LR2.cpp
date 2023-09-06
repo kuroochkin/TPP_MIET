af2015f777c7e758d5db2fe4f1037752ab9f27ce
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-06 19:26:16 +0300

    Добавьте файлы проекта.

---
+++ LR2/LR2.cpp
@@ -0,0 +1,36 @@
+﻿#include <omp.h>
+#include <stdio.h>
+#include <iostream>
+
+const int NMAX = 90;
+const int LIMIT = 90;
+
+void main()
+{
+    int i, j;
+    float sum;
+    float a[NMAX][NMAX];
+
+    for (i = 0; i < NMAX; i++)
+        for (j = 0; j < NMAX; j++)
+            a[i][j] = i + j;
+
+    double start = omp_get_wtime();
+#pragma omp parallel shared(a) if (NMAX>LIMIT)
+    {
+#pragma omp for private(i,j,sum) 
+        for (i = 0; i < NMAX; i++)
+        {
+            sum = 0;
+            for (j = 0; j < NMAX; j++)
+                sum += a[i][j];
+            printf("Sum elements rows %d : %f\n", i, sum);
+        }
+    } /* Завершение параллельного фрагмента */
+
+    double end = omp_get_wtime();
+
+    std::cout << "Time " << ": " << (end - start) << std::endl;
+}
+
+
---
+++ TPP_MIET/main.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <vector>
+#include <random>
+#include <ctime>
+#include <omp.h>
+
+// Функция для подсчета количества единиц в бинарной записи числа
+int CountOnesInBinary(int num) {
+    int count = 0;
+    while (num > 0) {
+        if (num % 2 == 1) {
+            count++;
+        }
+        num /= 2;
+    }
+    return count;
+}
+
+int M = 600;
+int N = 600;
+
+int rowsCountAsync()
+{
+    std::vector<std::vector<int>> matrix(M, std::vector<int>(N));
+    std::mt19937 rng(static_cast<unsigned int>(std::time(nullptr)));
+    std::uniform_int_distribution<int> distribution(1, 100);
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = distribution(rng);
+        }
+    }
+
+    double start = omp_get_wtime();
+    // Вычисление суммы единиц в бинарной записи числа всех попарных произведений элементов для каждой строки
+#pragma omp parallel for
+    for (int i = 0; i < M; i++) {
+        int sum = 0;
+        for (int j = 0; j < N; j++) {
+            for (int k = j + 1; k < N; k++) {
+                int value = matrix[i][j] * matrix[i][k];
+                sum += CountOnesInBinary(value);
+            }
+        }
+        /*std::cout << "Summa " << i << ": " << sum << std::endl;*/
+    }
+
+    double end = omp_get_wtime();
+
+    std::cout << "TimeAsync " << ": " << (end - start) << std::endl;
+
+    return 0;
+}
+
+int rowsCount()
+{
+    std::vector<std::vector<int>> matrix(M, std::vector<int>(N));
+    std::mt19937 rng(static_cast<unsigned int>(std::time(nullptr)));
+    std::uniform_int_distribution<int> distribution(1, 100);
+
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = distribution(rng);
+        }
+    }
+
+    double start = omp_get_wtime();
+
+    for (int i = 0; i < M; i++) {
+        int sum = 0;
+        for (int j = 0; j < N; j++) {
+            for (int k = j + 1; k < N; k++) {
+                int value = matrix[i][j] * matrix[i][k];
+                sum += CountOnesInBinary(value);
+            }
+        }
+        /*std::cout << "Summa " << i << ": " << sum << std::endl;*/
+    }
+
+    double end = omp_get_wtime();
+
+    std::cout << "Time " << ": " << (end - start) << std::endl;
+
+    return 0;
+}
+
+int main() {
+    rowsCountAsync();
+    rowsCount();
+}
